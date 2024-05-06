2e6ff009f45b189f845edbd3b39e03e413328d1a
Pedro Marcello Vincenty <vincentypedro@gmail.com>
2024-05-06 17:41:52 -0400

    functions practice

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Follow along with the examples here
+const greeting = () => {
+    console.log("Hello")
+}
+
+greeting()
+
+const sayHiTo = (name) => {
+    console.log(`Hi, ${name}`)
+}
+
+sayHiTo("pedro")
+
+const greetPerson = (name, greeting) => {
+    console.log(`${greeting}, ${name}`)
+}
+
+greetPerson("pedro", "Howdy")
+greetPerson("gabe", "sup")
+
+const getTip = (bill) => {
+    return "$" + bill * .2
+}
+
+console.log(`${getTip(100)} tip for bill`)
